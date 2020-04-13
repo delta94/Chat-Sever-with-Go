dd2957a251fb1887bdc474aaddf9a586859c4ff6
지농 <jinhong0719@naver.com>
2020-04-13 18:31:05 +0900

    [ADD] <server/server.go> 인터페이스의 각각의 메서드에 대한 설명 추가

--- server/server.go
+++ server/server.go
@@ -2,8 +2,11 @@ package server
 
 // 서버의 동작을 명확하게 정의하기 위해 정의한 인터페이스
 type ChatServer interface {
+	// Listen 메서드는 외부에서 들어오는 연결을 수신한다.
 	Listen(address string) error
+	// BroadCast 메서드는 다른 모든 클라이언트에게 명령을 보낸다.
 	BroadCast(command interface{}) error
+	// StartServer와 CloseServer 메서드는 서버를 시작하고 종료시킨다.
 	StartServer()
-	Close()
+	CloseServer()
 }
