d867aad9eae8ee1347dd803344d035223ad4df67
지농 <jinhong0719@naver.com>
2020-04-13 18:32:17 +0900

    [ADD] <server/tcp_server.go> 서버를 대기시켜 연결을 수신하기 위한 Listen 메서드

--- server/tcp_server.go
+++ server/tcp_server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"chat.server.com/protocol"
+	"log"
 	"net"
 	"sync"
 )
@@ -16,4 +17,15 @@ type client struct {
 	conn net.Conn
 	name string
 	writer *protocol.CommandWriter
+}
+
+func (ts *TcpChatServer) Listen(address string) (err error) {
+	var l net.Listener
+	if l, err = net.Listen("tcp", address); err != nil {
+		return
+	}
+
+	log.Printf("Listening on %v", address)
+	ts.listener = l
+	return
 }
