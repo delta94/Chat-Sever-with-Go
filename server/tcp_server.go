de670c403f5de2830da5296a1f393a2c8c2eecc1
지농 <jinhong0719@naver.com>
2020-04-13 18:38:21 +0900

    [ADD] <server/tcp_server.go> 서버와 클라이언트 객체의 각각의 필드에 대한 설명 추가

--- server/tcp_server.go
+++ server/tcp_server.go
@@ -8,19 +8,20 @@ import (
 )
 
 type TcpChatServer struct {
-	listener net.Listener
-	clients []*client
-	mutex *sync.Mutex
+	listener net.Listener	// 연결을 수신하기 위해 서버를 대기시키 위한 필드
+	clients []*client		// 연결되어있는 모든 클라이언트에 대한 정보를 담기 위한 필드
+	mutex *sync.Mutex		// race condition을 다루기 위한 mutex 필드
 }
 
 type client struct {
-	conn net.Conn
-	name string
-	writer *protocol.CommandWriter
+	conn net.Conn						// 클라이언트와 연결에 대한 정보를 담아두기 위한 필드
+	name string							// 클라이언트의 사용 이름을 저장시키기 위한 필드
+	writer *protocol.CommandWriter		// 클라이언트 측에 명령을 보내기 위한 필드
 }
 
 func (ts *TcpChatServer) Listen(address string) (err error) {
 	var l net.Listener
+	// net.Listen 함수를 이용하여 해당 주소에 서버를 대기시키기 위한 객체를 얻을 수 있다.
 	if l, err = net.Listen("tcp", address); err != nil {
 		return
 	}
@@ -29,3 +30,4 @@ func (ts *TcpChatServer) Listen(address string) (err error) {
 	ts.listener = l
 	return
 }
+
